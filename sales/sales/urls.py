6325e4b1e47d0c343dbda70b5e6407e07498927b
Shafiqul Islam <shafiqul.islam@karatbars.com>
2022-06-14 00:11:27 +0600

    Update anchor link for logout

--- sales/sales/urls.py
+++ sales/sales/urls.py
@@ -6,7 +6,6 @@ from django.contrib import admin
 from django.urls import path
 from app import views_orders
 from app import views_users
-from app import views
 from sales import settings
 from django.conf.urls.static import static
 
@@ -15,10 +14,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/orders/', views_orders.orders),
     path('api/orders/<int:order_id>/', views_orders.order),
-    path('', views.index_view),
-    path('orderPage', views.sales_view),
-    path('profile', views.profile_view),
-    path('signup', views.signup_view),
     path('api/signup/', views_users.signup),
     path('api/login/', views_users.login),
     path('api/user_data/<int:user_id>/', views_users.user_data),
